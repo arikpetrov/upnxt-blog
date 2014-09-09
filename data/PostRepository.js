d9694c8a276f01408af00242318b43928808e948
upnxt <chelfers@upnxt.com>
2014-09-08 20:58:51 -0500

    fixing repo callback issues

--- data/PostRepository.js
+++ data/PostRepository.js
@@ -3,9 +3,9 @@ var Repository = require( "./Repository" );
 
 var PostRepository = function ()
 {
-    var repo = new Repository();
+    var repo = new Repository( );
     var post = {};
-
+    
     function buildPost( row )
     {
         if ( row )
@@ -22,9 +22,9 @@ var PostRepository = function ()
                 description: row.MetaDescription
             };
         }
-    };
-
-
+    }    ;
+    
+    
     function buildReferences( rows )
     {
         if ( rows )
@@ -32,15 +32,15 @@ var PostRepository = function ()
             for ( var i = 0; i < rows.length; i++ )
             {
                 var reference = rows[i].Reference;
-
+                
                 if ( reference )
                     post.references.push( reference );
             }
         }
-    };
-
-
-    function buildNextPrev( position, row )
+    }    ;
+    
+    
+    function buildNextPrev( position, row, callback )
     {
         if ( row )
         {
@@ -50,9 +50,11 @@ var PostRepository = function ()
                 linkText: row.LinkText
             };
         }
-    };
-
-
+        
+        callback( post );
+    }    ;
+    
+    
     function getNextPrev( db, url, callback )
     {
         db.get( "SELECT Url, Title, LinkText \
@@ -62,10 +64,10 @@ var PostRepository = function ()
                     AND CreatedOn > (SELECT CreatedOn FROM POSTS WHERE url = $url) \
                 ORDER BY CreatedOn \
                 LIMIT 1", { $url: url }, function ( err, next )
+        {
+            
+            buildNextPrev( "next", next, function ( post )
             {
-                if ( next )
-                    buildNextPrev( "next", next );
-
                 db.get( "SELECT Url, Title, LinkText \
                      FROM POSTS \
                      WHERE \
@@ -73,25 +75,28 @@ var PostRepository = function ()
                         AND CreatedOn < (SELECT CreatedOn FROM POSTS WHERE url = $url) \
                      ORDER BY CreatedOn DESC \
                      LIMIT 1", { $url: url }, function ( err, prev )
+                {
+                    
+                    buildNextPrev( "prev", prev, function ( post )
                     {
-                        if ( prev )
-                            buildNextPrev( "prev", prev );
-
-                        callback();
-                    });
-            });
-    };
-
-
+                        callback( post );
+                    } );
+                } );
+            } );
+            
+        } );
+    }    ;
+    
+    
     //public methods
     return {
-
+        
         getLatest: function ( callback )
         {
             post = { next: null, prev: null, references: [] };
-
-      
-                repo.all( "SELECT \
+            
+            
+            repo.all( "SELECT \
                             P.Id, P.Url, P.Title, P.Body, P.LinkText, P.MetaTitle, P.MetaKeywords, P.MetaDescription, P.CreatedOn, P.GithubLink, \
                             PR.Reference \
                        FROM POSTS P \
@@ -100,30 +105,29 @@ var PostRepository = function ()
                                 P.IsPublished = 1 \
                             AND P.Id = (SELECT Id FROM POSTS ORDER BY CreatedOn DESC LIMIT 1) \
                        ORDER BY P.CreatedOn DESC", function ( err, rows )
+            {
+                if ( rows && rows.length > 0 )
+                {
+                    buildPost( rows[0] );
+                    buildReferences( rows );
+                    
+                    getNextPrev( repo, rows[0].Url, function ( p )
                     {
-                        if ( rows && rows.length > 0 )
-                        {
-                            buildPost( rows[0] );
-                            buildReferences( rows );
-
-                            getNextPrev( repo, rows[0].Url, function ()
-                            {
-                                callback( null, post );
-                            });
-                        }
-
-                        callback(null, post);
-                    });
+                        callback( null, p );
+                    } );
+                }
+                else
+                    callback( null, post );
+            } );
             
         },
-
+        
         getByUrl: function ( url, callback )
         {
             post = { next: null, prev: null, references: [] };
-
-            repo.serialize( function ( db )
-            {
-                db.all( "SELECT \
+            
+            
+            repo.query( "SELECT \
                             P.Id, P.Url, P.Title, P.Body, P.LinkText, P.MetaTitle, P.MetaKeywords, P.MetaDescription, P.CreatedOn, P.GithubLink, \
                             PR.Reference \
                        FROM POSTS P \
@@ -132,19 +136,19 @@ var PostRepository = function ()
                                 P.IsPublished = 1 \
                             AND P.Url = $url \
                        ORDER BY P.CreatedOn DESC", { $url: url }, function ( err, rows )
+            {
+                if ( rows && rows.length > 0 )
+                {
+                    buildPost( rows[0] );
+                    buildReferences( rows );
+                    
+                    getNextPrev( repo, url, function ()
                     {
-                        if ( rows && rows.length > 0 )
-                        {
-                            buildPost( rows[0] );
-                            buildReferences( rows );
+                        callback( null, post );
+                    } );
+                }
+            } );
 
-                            getNextPrev( db, url, function ()
-                            {
-                                callback( null, post );
-                            });
-                        }
-                    });
-            });
         }
     }
 };
--- data/Repository.js
+++ data/Repository.js
@@ -1,39 +1,47 @@
 ﻿var fs = require( "fs" );
 var sqlite3 = require( "sqlite3" );
 
-var Repository = function ( callback ) 
+var Repository = function ( callback )
 {
     var database_name = "./data/upnxt.db";
     var dbInstance = null;
-
+    
     fs.exists( database_name, function ( exists )
     {
         if ( !exists )
             throw ( "Database not found" );
-    });
-
+    } );
+    
     function using( callback )
     {
         dbInstance = new sqlite3.cached.Database( database_name );
         callback( dbInstance );
-    };
-
+    }    ;
+    
     return {
-
+        
         all: function ( query, callback )
         {
             using( function ( db )
             {
                 db.all( query, callback );
-            });
+            } );
         },
-
+        
+        query: function ( query, data, callback )
+        {
+            using( function ( db )
+            {
+                db.all( query, data, callback );
+            } );
+        },
+        
         get: function ( query, data, callback )
         {
             using( function ( db )
             {
                 db.get( query, data, callback );
-            });
+            } );
         }
     }
 };
