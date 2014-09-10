3d46bcb33733438528c40f0e4f50f15615989fa8
upnxt <cl0wnshoes@upnxt.com>
2014-09-09 22:11:39 -0500

    adding more code highlighting

--- public/js/app/postViewModel.js
+++ public/js/app/postViewModel.js
@@ -4,18 +4,20 @@
     {
         $( "ol.code li, span.code.inline" ).each( function ()
         {
-            var line = $( this ).text();
-
+            var line = $( this ).html();
             var s_pattern = /(".*?")/gi;
+            var c_pattern = /(\[.*?\])/gi;
             var tag_pattern = /(<.*?>)/gi;
             var at_pattern = /@/gi;
-            var fn_pattern = /(var )|(new )|(if )|(throw )|(function)|(return)/gi;
+            var fn_pattern = /(var )|(new )|(if )|(throw )|(function)|(return)|(public )|(namespace )|( get)|( set)|(long)|( string)|(class )|(where)(void )|(using )|(private readonly)/gi;
+            var val_pattern = /\d/gi;
+            var interface_pattern = /\bI\S*/g;
 
             if ( line.match( tag_pattern ) )
             {
                 line = line.replace( tag_pattern, function ( match, token )
-                {
-                    return '<span class="tag">' + match.replace( "<", "&lt;" ).replace( ">", "&gt;" ) + '</span>';
+                {                  
+                    return '<span class="tag">' + match.replace("<", "&lt;").replace(">", "&gt;") + '</span>';
                 });
             }
 
@@ -23,7 +25,7 @@
             {
                 line = line.replace( s_pattern, function ( match, token )
                 {
-                    if (match.indexOf('"tag"') > -1 || match.indexOf('"p"') > -1)
+                    if (match.indexOf('"tag"') > -1 || match.indexOf('"p"') > -1 || match.indexOf('"c"') > -1)
                         return match;
 
                     return '<span class="s">' + match + '</span>';
@@ -43,6 +45,24 @@
                 line = line.replace( fn_pattern, function ( match, token )
                 {
                     return '<span class="fn">' + match + '</span>';
+                });
+            }
+            
+            if (line.match(val_pattern)) {
+                line = line.replace(val_pattern, function (match, token) {
+                    return '<span class="val">' + match + '</span>';
+                });
+            }
+            
+            if (line.match(interface_pattern)) {
+                line = line.replace(interface_pattern, function (match, token) {
+                    return '<span class="i">' + match + '</span>';
+                });
+            }
+            
+            if (line.match(c_pattern)) {
+                line = line.replace(c_pattern, function (match, token) {
+                    return '<span class="c">' + match.replace("[", "").replace("]","") + '</span>';
                 });
             }
 
